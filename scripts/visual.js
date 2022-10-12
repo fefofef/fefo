e8a87c0f5373c9d21ac2885a737c9b3e6dc398b2
ofefo <fefobrandao@gmail.com>
2022-10-11 23:47:54 -0300

    Update

--- scripts/visual.js
+++ scripts/visual.js
@@ -1,23 +1,48 @@
-var ball = {
-	x: 800,
-	y: Math.random()*,
-	xspeed: -1*Math.random(0,10)
-}
+let ballSize = 50;
+let ballX = Math.random()*800;
+let ballY = Math.random()*800;
+let width = 800;
+let height = 800;
+let ballxSpeed = 1;
+let ballySpeed = 1;
+var ballColors = [Math.random()*255,Math.random()*255,Math.random()*255];
 
 function setup() {
-	let renderer = createCanvas(800,800);
+	let renderer = createCanvas(width, height);
 	renderer.parent("canvas-container");
 }
 
-function drawEllipse() {
-	stroke(255);
-	strokeWeight(1);
-	fill(0);
-	ellipse(ball.x, ball.y+random(0,100), 100, 100);
-	x = ball.x+ball.xspeed;
+class Ball {
+	constructor() {
+	}
 }
 
+function drawEllipse(x, y) {
+	stroke(255);
+	strokeWeight(1);
+	fill(ballColors);
+	ellipse(x, y, 50);
+}
+
+
 function draw() {
-	background(233, 191, 105);
-	drawEllipse();
+	background(100, 100, 100);
+	ballX += ballxSpeed;
+	ballY += ballySpeed;
+	ballColors[0] = ((ballY/800)*255);
+	drawEllipse(ballX, ballY);
+	if (ballX > width-ballSize/2 || ballX < ballSize/2) {
+		print("BallX: " + ballX);
+		ballxSpeed = (ballxSpeed + random(15)*0.2)*-1;
+		print("ballxSpeed: " + ballxSpeed);
+	}
+	if (ballY > height-ballSize/2 || ballY < ballSize/2) {
+		print("BallY: " + ballY);
+		ballySpeed = (ballySpeed + random(15)*0.2)*-1;
+		print("ballySpeed: " + ballySpeed);
+	}
+	if (ballY < -1 || ballX < -1) {
+		ballX = ballX + ballSize;
+		ballY = ballY + ballSize; 
+	}
 }
