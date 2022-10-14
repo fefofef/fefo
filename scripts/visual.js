eb50c9108711f5cd1b8c93d459b08b178ea345a7
ofefo <fefobrandao@gmail.com>
2022-10-13 21:11:20 -0300

    Update visual

--- scripts/visual.js
+++ scripts/visual.js
@@ -1,69 +1,145 @@
+//CANVAS SIZE
 let width = 800;
 let height = 800;
 
-var x, y, acc1, acc2; //ball propierties
-
+//VISUAL ELEMENTS
 let balls = [];
+let lines = [];
+
+var lineGrowing = 0;
+var destroyB = 11;
+var generator = 0;
 
 function setup(){
 	let renderer = createCanvas(width, height);
 	renderer.parent("canvas-container");
 	
 	background(100);
-
-	frameRate(24);
-
+	frameRate(25);
 }
 
 function draw() {
 	background(100);
+	
+	generator = random();
+	makeBall();
+	if (lineGrowing == 0) {
+		makeLine();
+	}
+	
+	for (let line of lines) {
+		line.grow();
+		line.show();
+	}
+
+	for (let i = 0; i < lines.length; i++) {
+		lines[i].grow();
+		lines[i].show();
+		if (lines[i].growing == 1) {
+			lineGrowing = 1;	
+		}
+		else {
+			lineGrowing = 0;
+		}
+	}
+
 	for (let ball of balls) {
 		ball.move();
 		ball.show();
 	}
-	
+
+	destroyB = 11 - balls.length;
+
 	for (let i = 0; i < balls.length; i++) {
 		balls[i].move();
 		balls[i].show();
-		if (balls[i].n > 10) {
-			balls.splice(i, 1);	
+		if (balls[i].n > destroyB) {
+			balls.splice(i, 1);
 		}
 	}
 }
 
-function mousePressed () {
-	let ball = new Ball(mouseX, mouseY);
-	balls.push(ball);
-};
+function makeBall(randomNumber) {
+	if (balls.lenght < 1) {
+		if (generator > 0.90) {
+			let ball = new Ball(random(width), random(height));
+			balls.push(ball);
+		}
+	}
+	else if (balls.length < 2) {
+		if (generator > 0.99) {
+			let ball = new Ball(random(width), random(height));
+			balls.push(ball);
+		}
+	}
+	else if (balls.length < 5) {
+		if (generator > 0.995) {
+			let ball = new Ball(random(width), random(height));
+			balls.push(ball);
+		}
+	}
+	else {
+		if (generator > 0.999) {
+			let ball = new Ball(random(width), random(height));
+			balls.push(ball);
+		}
+	}
+}
+
+function makeLine(randomNumber) {
+	if (lines.lenght < 1) {
+		if (generator < 0.1) {
+			let line = new Line();
+			lines.push(line);
+		}
+	}
+	else if (lines.lenght < 20) {
+		if (generator < 0.05) {
+			let line = new Line();
+			lines.push(line);
+		}
+	}
+	else {
+		if (generator < 0.01) {
+			let line = new Line();
+			lines.push(line);
+		}
+	}
+}
+
+//function mousePressed () {
+//	let ball = new Ball(mouseX, mouseY);
+//	balls.push(ball);
+//};
 
 class Ball {
 	constructor(x, y) {
 		this.x = x; //position X
 		this.y = y; //position Y
 
-		this.acc1 = random(-10, 10); //accX
-		this.acc2 = random(-10, 10); //accY
+		this.acc1 = random(-5, 5); //accX
+		this.acc2 = random(-5, 5); //accY
 
 		this.c = color(255, random(184), random(77)); //color
-		this.s = random(10, 100); //size
+		this.s = random(70, 100); //size
 		this.n = 0; //number of collisions
 	}
 
 	move() {
-		if (this.x > width) {
-			this.acc1 = random(10)*-1;
+		if (this.x + (this.s/2) > width) {
+			this.acc1 = random(5)*-1;
 			this.n += 1;
 		}
-		else if (this.x < 0) {
-			this.acc1 = 10;		
+		else if (this.x - (this.s/2) < 0) {
+			this.acc1 = random(5);		
 			this.n += 1;
 		}
-		if (this.y > height) {
-			this.acc2 = random(10)*-1;
+		if (this.y + (this.s/2) > height) {
+			this.acc2 = random(5)*-1;
 			this.n += 1;
 		}
-		else if (this.y < 0) {
-			this.acc2 = random(10);		
+		else if (this.y - (this.s/2) < 0) {
+			this.acc2 = random(5);		
 			this.n += 1;
 		}
 		this.x = this.x + this.acc1;
@@ -76,4 +152,44 @@ class Ball {
 		fill(32);
 		ellipse(this.x, this.y, this.s);
 	}
+}
+
+class Line {
+	constructor() {
+		this.x1 = random(width);
+		this.y1 = random(height);
+		this.x2 = this.x1;
+		this.y2 = this.y1;
+
+		this.xg = random(-1, 1); //growth
+		this.yg = random(-1, 1); //growth
+		this.sW = random(1,10); //stroke Weight
+
+		this.size = random(height,width); //line lenght 
+		this.growing = 0;
+	}
+
+	grow(){
+		if (dist(this.x1, this.y1, this.x2, this.y2) < this.size) {
+			this.growing = 1;
+			this.x2 += this.xg;
+			this.y2 += this.yg;
+		} 
+		else if (this.x2 == width || this.y2 == height || this.x2 == 0 || this.y2 == 0) {
+			this.growing = 0;
+			this.xg = 0;
+			this.yg = 0;
+		} else {
+			this.growing = 0;
+			this.xg = 0;
+			this.yg = 0;
+		}
+	}
+
+	show() {
+		stroke(0);
+		strokeWeight(this.sW);
+		line(this.x1, this.y1, this.x2, this.y2);
+	}
+	
 }
